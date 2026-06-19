340b3aefc54d9392e606707a6e3eacf9d395a276
rajsmahajan <mahajanraj0210@gmail.com>
2026-06-19 10:19:20 +0530

    added to find price(with 18% of GST) of any item.

--- Hello.c
+++ Hello.c
@@ -2,34 +2,10 @@
 #include<math.h>
 
 int main() {
-  int n, i, isprime=1;
-
-  printf("enter number :");
-  scanf("%d", &n);
-
-  if(n<=1) {
-    isprime = 0;
-  } 
-  else if(n==2) {
-    isprime = 1;
-  } 
-  else if(n % 2 == 0) {
-    isprime = 0;
-  }
-  else{
-    for(int i=3; i<= sqrt(n); i = i + 2 ) {
-      if(n % i == 0) {
-        isprime = 0;
-        break;
-      }
-    }
-  }
-
-  if(isprime == 1) {
-    printf("%d is PRIME number.  \n", n);
-  } else {
-    printf("%d is NOT PRIME number. \n", n);
-  }
-
-  return 0;
+    float value;
+    printf("enter a value :");
+    scanf("%f", &value);
+    value = value + (0.18 * value);
+    printf("final price(with GST) is : %f", value); 
+    return 0;
 }

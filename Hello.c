c31ffb81629a772f9f7cf48021c39d230418a2cb
rajsmahajan <mahajanraj0210@gmail.com>
2026-06-24 10:35:40 +0530

    Added to find square of any whole number.

--- Hello.c
+++ Hello.c
@@ -1,37 +1,10 @@
 #include<stdio.h>
 #include<math.h>
 
-float triangleArea(float x, float y);
-float rectangleArea(float a, float b);
-
-/* to find area of triangle use x and y.
-   to find area of recrangle use a and b.
-*/
-
 int main() {
-    printf("-:Area of Triangle:- \n");
-    float x, y;
-    printf("enter x :");
-    scanf("%f", &x);
-    printf("enter y :");
-    scanf("%f", &y);
-    printf("Area of triangle is : %f \n", triangleArea(x,y));
-
-
-    printf("-:Area of Rectangle:- \n");
-    float a, b;
-    printf("enter a : ");
-    scanf("%f", &a);
-    printf("enter b : ");
-    scanf("%f", &b);
-    printf("Area of rectangle is : %f \n", rectangleArea(a,b));
-
+    int n;
+    printf("enter value : ");
+    scanf("%d", &n);
+    printf("square is : %f \n", pow(n,2));
     return 0;
-}
-
-float triangleArea(float x, float y) {
-    return 0.5 * x * y;
-}
-float rectangleArea(float a, float b) {
-    return a * b;
 }

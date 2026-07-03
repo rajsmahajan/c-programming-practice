e5f312a13bd16526c469201835a27e6582632709
rajsmahajan <mahajanraj0210@gmail.com>
2026-07-03 11:16:10 +0530

    Added to calculate percentage

--- Hello.c
+++ Hello.c
@@ -1,17 +1,28 @@
 #include<stdio.h>
 
-float convertTemp(float celsius);
+float calcuPersentage(float science, float math, float sanskrit, float english, float marathi);
 
 int main() {
-    float n;
-    printf("enter celsius : ");
-    scanf("%f", &n);
-    float far = convertTemp(n);
-    printf("Fahrenheit : %f \n", far);
+    float science, math, sanskrit, english, marathi;
+    printf("enter marks in science :");
+    scanf("%f", &science);
+
+    printf("enter marks in math : ");
+    scanf("%f", &math);
+
+    printf("enter marks in sanskit : ");
+    scanf("%f", &sanskrit);
+
+    printf("enter marks in english :");
+    scanf("%f", &english);
+
+    printf("enter marks in marathi : ");
+    scanf("%f", &marathi);
+
+    printf("persentage is : %f \n ", calcuPersentage(science, math, sanskrit, english, marathi));  
     return 0;
 }
 
-float convertTemp(float celsius) {
-    float far = celsius * (9.0/5.0) + 32;
-    return far;
+float calcuPersentage(float science, float math, float sanskrit, float english, float marathi) {
+    return ((science + math + sanskrit + english + marathi) / 5.0 );  
 }

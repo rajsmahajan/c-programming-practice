9217b867aa8b181c7e1c651e8bdac71616a96ab1
rajsmahajan <mahajanraj0210@gmail.com>
2026-07-03 14:55:10 +0530

    Added a practice of pointer.

--- Hello.c
+++ Hello.c
@@ -1,25 +1,10 @@
 #include<stdio.h>
 
-int fib(int n);
-
 int main() {
-    int n;
-    printf("enter  number : ");
-    scanf("%d", &n);
-    printf("%d",fib(n));
+    int age = 55;
+    int *ptr = &age;
+    int _age = *ptr;
+
+    printf("%d", _age);
     return 0;
-}
-
-int fib(int n) {
-    if(n == 0) {
-        return 0;
-    }
-    if(n == 1) {
-        return 1;
-    }   
-
-    int fibNm1 = fib(n-1);
-    int fibNm2 = fib(n-2);
-    int fibN = fib(n-1) + fib(n-2);
-    return fibN;
 }

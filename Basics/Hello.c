16df240e70dff1ba292f8995fcebe6b037fa584d
rajsmahajan <mahajanraj0210@gmail.com>
2026-08-09 16:32:24 +0530

    added to Print 1-6 with help of Arrays.

--- Basics/Hello.c
+++ Basics/Hello.c
@@ -1,22 +1,17 @@
 #include<stdio.h>
 
+void printNumbers(int arr[], int n);
+
 int main() {
-    int Items[3]; 
-    int total;
-    printf("Enter Price of Item 1 : ");
-    scanf("%d", &Items[0]);
+    int arr[] = {1,2,3,4,5,6};
+    printNumbers(arr, 6);
 
-    printf("Enter Price of Item 2 : ");
-    scanf("%d", &Items[1]);
+    return 0;}
 
-    printf("Enter Price of Item 3 : ");
-    scanf("%d", &Items[2]);
 
-    printf("Item 1 = %d, Item 2 = %d, Item 3 =%d\n", Items[0], Items[1], Items[2]);
-
-    total = Items[0]+Items[1]+Items[2];
-    printf("Final Value(with GST):%d\n", total + total * 18 / 100);
-    printf("Thank You:)");
-
-    return 0;
+void printNumbers(int arr[], int n) {
+    for(int i=0; i < n; i++) {
+        printf("%d \t", arr[i]);
+    }
+    printf("\n");
 }

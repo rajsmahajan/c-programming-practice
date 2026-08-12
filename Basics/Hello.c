2c507df745678e4cc40e310ee18bcde3db5b39c7
rajsmahajan <mahajanraj0210@gmail.com>
2026-08-12 14:44:28 +0530

    added to find total primte number.

--- Basics/Hello.c
+++ Basics/Hello.c
@@ -1,17 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int countOdd(int arr[], int n);
+int isPrime(int num);
+int countPrime(int arr[], int n);
 
 int main() {
-    int arr[] = {1, 2, 5, 8, 9, 65, 87, 88, 68, 45, 46, 22, 3, 44, 64, 58};
-    printf("%d", countOdd(arr, 16));
+    int arr[] = {1, 2, 5, 8, 9, 65, 87, 88, 68, 45, 46, 22, 3, 58};
+    
+    printf("%d", countPrime(arr, 16));
     return 0;
 }
 
-int countOdd(int arr[], int n) {
+int isPrime(int num) {
+    if (num <= 1) return 0;
+
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int countPrime(int arr[], int n) {
     int count = 0;
-    for(int i=0; i < n; i++) {
-        if(arr[i] % 2 == 0){
+    for (int i = 0; i < n; i++) {
+        if (isPrime(arr[i])) {
             count++;
         }
     }
